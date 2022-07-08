4bf40b9ba4636a50a2b317c8e69748287547f562
FranciscoMaro <francisco.maro@outlook.com.ar>
2022-07-08 15:49:11 -0300

    Nuevo project

---
+++ main.js
@@ -0,0 +1,38 @@
+let Nombre= prompt("decime tu nombre");
+
+let Apellido= prompt ("decime tu apellido");
+
+let NumeroDeCompetidor= parseInt(prompt("Ingrese su numero de competidor"));
+
+let camiseta = 0;
+
+
+alert ("Bienvenido " + " " + Nombre + " " + Apellido + " al ingreso" );
+
+if (NumeroDeCompetidor <= 50) {
+   alert ("sos del grupo A")
+
+
+
+/* ciclo */
+for (let i = 0; i < camiseta.length; i++) {
+  text += camiseta[i] + "<br>";
+  console.log(text)
+}
+
+for (let i= 1; i < 50; i++) { 
+
+    alert ("Te vas al grupo A")
+  let resultado= NumeroDeCompetidor + i;
+  alert (NumeroDeCompetidor + "x" + i +  " = " + resultado)
+}
+}
+
+else {
+    alert ( " sos del Grupo B")
+}
+
+console.log (Nombre);
+console.log (Apellido);
+console.log ( NumeroDeCompetidor)
+
